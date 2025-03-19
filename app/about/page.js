10ea279685dc9a33c77852023b1223fd308c6025
something-man <minnalkodi735@gmail.com>
2025-03-19 17:48:43 +0530

    build

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Link as ScrollLink } from "react-scroll";
@@ -5,7 +6,6 @@ import { Link as ScrollLink } from "react-scroll";
 export default function AboutPage() {
   return (
     <div id="about" className="min-h-screen flex flex-col md:flex-row  items-center justify-center bg-gray-900 text-white p-6">
-      {/* <div className="w-full md:w-2/3 lg:w-3/4 flex flex-col md:flex-row bg-gray-800 p-6 rounded-lg shadow-lg"> */}       
         <div className="hidden md:flex w-full md:w-1/2 flex pl-3 md:mb-15 justify-center items-center">
           <Image 
             src="/profile.jpeg"
@@ -15,7 +15,7 @@ export default function AboutPage() {
             className=" w-28 h-28 md:w-64 md:h-64 lg:w-78 lg:h-78 rounded-full object-cover border-4 border-blue-400 duration-300 shadow-[0px_0px_10px_3px_rgba(135,206,250,0.6)]"
           />
         </div>
-<div className="w-full md:mb-15 md:w-2/3 mt-6 md:mt-0 md:pl-8 md:w-2/2 md:text-left  md:w-1/3.5 lg:w-6/8 pr-17">
+        <div className="w-full md:mb-15 md:w-2/3 mt-6 md:mt-0 md:pl-8 md:w-2/2 md:text-left  md:w-1/3.5 lg:w-6/8 pr-17">
           <h1 className=" text-3xl mt-6 md:text-4xl font-extrabold text-blue-400">About Me</h1>
           <h2 className="text-lg  md:text-xl text-gray-100 font-medium mt-2">Frontend Developer</h2>
 
